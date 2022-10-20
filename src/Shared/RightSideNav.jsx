6222f9050a0d018a8894b722c1a49ef225de07f6
tahdi-islam <tshdi60@gmail.com>
2022-10-20 13:10:08 +0600

    add right side list

--- src/Shared/RightSideNav.jsx
+++ src/Shared/RightSideNav.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import { Button, ButtonGroup } from 'react-bootstrap';
+import { Button, ButtonGroup, ListGroup } from 'react-bootstrap';
+import {FaGoogle, FaGithub, FaFacebook, FaTwitter, FaTwitch, FaWhatsapp} from "react-icons/fa";
 
 const RightSideNav = () => {
     return (
-        <ButtonGroup vertical>
-            <Button variant="outline-primary">Log in with Google</Button>
-            <Button variant="outline-dark">Log in with GitHub</Button>
-        </ButtonGroup>
+        <div>
+            <ButtonGroup vertical>
+                <Button className='mb-2' variant="outline-primary"><FaGoogle /> Log in with Google</Button>
+                <Button variant="outline-dark"><FaGithub /> Log in with GitHub</Button>
+            </ButtonGroup>
+            <div className="mt-3 mb-2">
+                <h4>Find us on</h4>
+                <ListGroup>
+                    <ListGroup.Item className='mb-2'><FaFacebook/> Facebook</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaTwitter/> Twitter</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaTwitch/> Twitch</ListGroup.Item>
+                    <ListGroup.Item className='mb-2'><FaWhatsapp/> Whatsapp</ListGroup.Item>
+                </ListGroup>
+            </div>
+        </div>
     );
 };
 
