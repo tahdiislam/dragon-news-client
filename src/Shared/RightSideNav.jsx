248a3a8ef000f63f312190ce2a6d7783fee24dc1
tahdi-islam <tshdi60@gmail.com>
2022-10-20 13:19:20 +0600

    add carousel

--- src/Shared/RightSideNav.jsx
+++ src/Shared/RightSideNav.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Button, ButtonGroup, ListGroup } from 'react-bootstrap';
 import {FaGoogle, FaGithub, FaFacebook, FaTwitter, FaTwitch, FaWhatsapp} from "react-icons/fa";
+import Carousel from 'react-bootstrap/Carousel';
+import Brand1 from '../assets/brands/Brand1.jpg'
+import Brand2 from '../assets/brands/Brand2.png'
 
 const RightSideNav = () => {
     return (
@@ -17,6 +20,26 @@ const RightSideNav = () => {
                     <ListGroup.Item className='mb-2'><FaTwitch/> Twitch</ListGroup.Item>
                     <ListGroup.Item className='mb-2'><FaWhatsapp/> Whatsapp</ListGroup.Item>
                 </ListGroup>
+            </div>
+            <div>
+                <Carousel>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={Brand1}
+                            alt="First slide"
+                            height={150}
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={Brand2}
+                            alt="Second slide"
+                            height={150}
+                        />
+                    </Carousel.Item>
+                </Carousel>
             </div>
         </div>
     );
